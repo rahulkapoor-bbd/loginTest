67433ed28aa107b7c98d0cc4adc86db97a9160ed
rahulkapoor-bbd <122476067+rahulkapoor-bbd@users.noreply.github.com>
2023-01-18 11:47:02 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ handleLogin = () => {
     return data.json()
 }).then(data => {console.log(data)})
 }
+
+body.onload = function() {
+    let paramString = urlString.split('?')[1];
+    let queryString = new URLSearchParams(paramString);
+
+    for (let pair of queryString.entries()) {
+   console.log("Key is: " + pair[0]);
+   console.log("Value is: " + pair[1]);
+}
+}
