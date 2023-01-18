761962227d24ceaec19cc03c510bd11d3bf06ee3
rahulkapoor-bbd <122476067+rahulkapoor-bbd@users.noreply.github.com>
2023-01-18 11:25:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ handleLogin = () => {
     const req = new Request("https://github.com/login/oauth/authorize")
     const headers = new Headers()
     headers.append('Access-Control-Allow-Origin', '*')
-   // headers.append('Origin', 'https://rahulkapoor-bbd.github.io/loginTest/')
+    headers.append('Origin', 'https://rahulkapoor-bbd.github.io/loginTest/')
     headers.append('client_id','fd064980a8a92a69b281')
     fetch("https://github.com/login/oauth/authorize",{
         method: 'GET',
