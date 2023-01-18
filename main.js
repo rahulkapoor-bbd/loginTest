78ca6d84d1ab708c5528c8d8a241fa470554056d
rahulkapoor-bbd <122476067+rahulkapoor-bbd@users.noreply.github.com>
2023-01-18 12:06:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -19,5 +19,19 @@ onBodyLoad = () => {
     for (let pair of queryString.entries()) {
    console.log("Key is: " + pair[0]);
    console.log("Value is: " + pair[1]);
+        if(pair[0] == 'code'){
+           fetch('https://github.com/login/oauth/access_token', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "client_id": "fd064980a8a92a69b281",
+                                    "client_secret": "fee177ef5644ef37b319ac177b88a6c68f2e1ace",
+                                    "code": pair[1]})
+        })
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
+        }
 }
 }
